f322711c9d9838fe7c171b46b9b41cff05cef2ea
Ibarra <aitoribarra@tutanota.com>
2022-01-18 17:10:47 +0100

    ADD - theme

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+export interface ThemeType {
+  backgroundColor: string;
+  primaryColor: string;
+  secondaryColor: string;
+  titleSize: string;
+  subtitleSize: string;
+  textSize: string;
+}
+
+export const theme: ThemeType = {
+  primaryColor: "#f2f2f2",
+  backgroundColor: "#282c34",
+  secondaryColor: "#61dafb",
+  titleSize: "3rem",
+  subtitleSize: "1.5rem",
+  textSize: "1rem",
+};
