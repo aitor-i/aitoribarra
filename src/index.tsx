0643eb9d7d9dc9521d7cd9c0e7039fedb919618d
Ibarra <aitoribarra@tutanota.com>
2022-02-02 20:43:22 +0100

    REFACTOR - App => Home

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./views/App";
+import App from "./views/Home/App";
 import About from "./views/About/About";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
