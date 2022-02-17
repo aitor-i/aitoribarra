06bd3f387d854454eb6810cfd53198de842466a1
Your Name <you@example.com>
2022-02-17 19:13:20 +0100

    ADD - GlassBox wapper

---
+++ src/components/GlassBox/GlassBox.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const GlassBoxStyles = styled.div`
+  background: rgba(255, 255, 255, 0.6);
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  border-radius: 10px;
+  backdrop-filter: blur(2px);
+`;
+
+export default GlassBoxStyles;
