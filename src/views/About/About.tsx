a5ee8c03f12d8ddfc36198e5a5d8c4ec5940eefc
Ibarra <aitoribarra@tutanota.com>
2022-02-18 11:42:27 +0100

    DELETE - dummy text

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -33,16 +33,6 @@ const About = () => {
       </ThemeProvider>
     </Fragment>
   );
-
-  function dummyText() {
-    return `
-        I must not fear. Fear is the mind-killer. Fear is the little-death that
-        brings total obliteration. I will face my fear. I will permit it to pass
-        over me and through me. And when it has gone past I will turn the inner
-        eye to see its path. Where the fear has gone there will be nothing. Only
-        I will remain.
-      `;
-  }
 };
 
 export default About;
